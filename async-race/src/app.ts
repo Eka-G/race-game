2686ebcc71e539d2b47fba167f1cdad67f56fabb
Eka-G <ekaglazkovaa@yandex.ru>
2021-06-16 10:39:55 +0300

    refactor: correct toggle pages

--- async-race/src/app.ts
+++ async-race/src/app.ts
@@ -19,18 +19,21 @@ class App {
         event.target === this.header.garageBtn.element &&
         this.garagePage.element.classList.contains('unactive-page')
       ) {
-        this.garagePage.element.classList.remove('unactive-page');
-        this.winnersPage.element.classList.add('unactive-page');
+        App.toggleClass(this.garagePage.element, this.winnersPage.element);
       }
 
       if (
         event.target === this.header.winnersBtn.element &&
         this.winnersPage.element.classList.contains('unactive-page')
       ) {
-        this.winnersPage.element.classList.remove('unactive-page');
-        this.garagePage.element.classList.add('unactive-page');
+        App.toggleClass(this.winnersPage.element, this.garagePage.element);
       }
     });
+  }
+
+  private static toggleClass(active: HTMLElement, unactive: HTMLElement) {
+    active.classList.remove('unactive-page');
+    unactive.classList.add('unactive-page');
   }
 }
 
